0732d2404444a3020996328e085f30655d19a1ed
Devanshi Sheth <devanshisheth@Devanshis-MacBook-Pro.local>
2022-04-02 16:19:48 -0400

    added dislike button and service

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,8 +1,19 @@
+import {useEffect, useState} from "react";
+import * as service from "../../services/dislikes-service";
+import Tuits from "../tuits";
+
 const MyDislikes = () => {
+    const [dislikedTuits, setdisLikedTuis] = useState([]);
+    const findTuitsIdislike = () =>
+        service.findAllTuitsDislikedByUser("me")
+            .then((tuits) => setdisLikedTuis(tuits));
+    useEffect(findTuitsIdislike, []);
+
     return(
         <div>
-            <h1>My Dislikes</h1>
+            <h2>My Dislikes</h2>
+            <Tuits tuits={dislikedTuits} refreshTuits={findTuitsIdislike}/>
         </div>
-    )
+    );
 }
 export default MyDislikes;
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,18 +1,41 @@
 import React, {useEffect, useState} from "react";
-import * as service from "../../services/likes-service";
+import * as likeservice from "../../services/likes-service";
+import * as securityservice from "../../services/security-service";
+import * as dislikeservice from "../../services/dislikes-service"
 
 
-
-const TuitStats = ({tuit, likeTuit = () => {}}) => {
-    const [likedTuits, setLikedTuis] = useState([]);
-    const findTuitsILike = () =>
-        service.findAllTuitsLikedByUser("me")
-            .then((tuits) => setLikedTuis(tuits));
-    useEffect(findTuitsILike, []);
-    const userLiked = false;
-    const checkIfUserLikedATuit = () => {
-        
+const TuitStats = ({tuit, likeTuit = () => {}, dislikeTuit = () => {}}) => {
+    const [liked, setLiked] = useState(false);
+    const [disliked, setDisliked] = useState(false);
+    const findTuitsILike = () =>{
+        likeservice.findAllTuitsLikedByUser("me")
+            .then((tuits) => {
+                const tuitLiked = tuits.find(findTuit => findTuit._id === tuit._id)
+                if(tuitLiked!==undefined){
+                    setLiked(true)
+                }
+            });
     }
+
+    const findTuitsIDislike = () =>{
+        dislikeservice.findAllTuitsDislikedByUser("me")
+            .then((tuits) => {
+                const tuitDisliked = tuits.find(findTuit => findTuit._id === tuit._id)
+                if(tuitDisliked!==undefined){
+                    setDisliked(true)
+                }
+            });
+    }
+
+    useEffect(async () => {
+        try {
+            const user = await securityservice.profile();
+            findTuitsILike()
+            findTuitsIDislike()
+        } catch (e) {
+        }
+    }, []);
+
     return (
       <div className="row mt-2">
         <div className="col">
@@ -28,26 +51,52 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
           }
         </div>
         <div className="col">
-          <span className="ttr-like-tuit-click" onClick={() => likeTuit(tuit)}>
-              {
-
+          <span className="ttr-like-tuit-click" onClick={() => {
+              likeTuit(tuit)
+              if(liked){
+                  setLiked(false)
               }
-                      <i className="fa-regular fa-thumbs-up" ></i>
+              else {
+                  setLiked(true)
+              }
+          }}>
+              {
+                  liked &&
+                  <i className="fa-solid fa-thumbs-up"  style={{color: 'blue'}}></i>
+              }
+              {
+                  !liked &&
+                  <i className="fa-regular fa-thumbs-up" ></i>
+              }
               {tuit.stats &&
                       <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
               }
                </span>
         </div>
           <div className="col">
-          <span className="ttr-like-tuit-click" onClick={() => likeTuit(tuit)}>
-              {
-                  <>
-                      <i className="fa-regular fa-thumbs-down" ></i>
-                      <span className="ttr-stats-likes">{tuit.stats && tuit.stats.likes}</span>
-                  </>
+          <span className="ttr-dislike-tuit-click" onClick={() => {
+              dislikeTuit(tuit)
+              if(disliked){
+                  setDisliked(false)
               }
-          </span>
+              else {
+                  setDisliked(true)
+              }
+          }}>
+              {
+                  disliked &&
+                  <i className="fa-solid fa-thumbs-down"  style={{color: 'red'}}></i>
+              }
+              {
+                  !disliked &&
+                  <i className="fa-regular fa-thumbs-down" ></i>
+              }
+              {tuit.stats &&
+                  <span className="ttr-stats-likes">{tuit.stats && tuit.stats.dislikes}</span>
+              }
+               </span>
           </div>
+
         <div className="col">
           <i className="far fa-inbox-out"></i>
         </div>
